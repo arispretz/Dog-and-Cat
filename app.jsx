6f2d05a4c44abf740da8b00f3a76475c190832c0
arispretz <117314460+arispretz@users.noreply.github.com>
2024-06-12 17:13:17 -0300

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,35 @@
+import "./styles/styles.css";
+import React, { useState } from 'react';
+
+const catImgUrl = 'https://cdn.glitch.global/d85dc994-ed5d-4667-916a-203be8cd4f91/thumbnails%2Fcat.jpg?1718217108894';
+const dogImgUrl = 'https://cdn.glitch.global/d85dc994-ed5d-4667-916a-203be8cd4f91/thumbnails%2Fdog.jpg?1718217129891';
+
+const App = () => {
+  // declarar estado
+  const [image, setImage] = useState(catImgUrl);
+
+  const changeAnimal = () => {
+    const result = image === catImgUrl ? dogImgUrl : catImgUrl;
+    setImage(result);
+  };
+
+  return (
+    <>
+    <div className='App'>
+      <h1>30 Days Of React</h1>
+      <div className='animal'>
+        <img src={image} alt='animal' />
+      </div>
+
+      <button onClick={changeAnimal} className='btn btn-add'>
+        Change
+      </button>
+    </div>
+    <footer className="footer">
+      <p>Created by Ariana Spretz</p>
+    </footer>
+    </>
+  );
+};
+
+export default App;
